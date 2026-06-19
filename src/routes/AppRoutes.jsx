fdf44b90361a648ebcc9ad83b7c81d819886e430
fabri98bazan <fabri98201098@gmail.com>
2026-06-19 14:28:32 -0300

    POST de clientes, y rutas, Bazan Fabricio

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import ListaClientes from "../pages/ListaClientes";
+import DetalleCliente from "../pages/DetalleCliente";
+import RutaProtegida from "../components/common/RutaProtegida";
+
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    element: <RutaProtegida />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/clientes",
+        element: <ListaClientes />,
+      },
+      {
+        path: "/clientes/:id",
+        element: <DetalleCliente />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Navigate to="/dashboard" replace />,
+  },
+]);
